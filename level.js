73f2674fa23a421434791942653d600370d2b265
Zachary Crandall <zach.t.crandall@gmail.com>
2015-02-28 14:18:58 -0800

    Re-added level.js with initializations

--- level.js
+++ level.js
@@ -1,7 +1,20 @@
 function Level() {
     
     this.ship= [ship0,ship1,ship2,ship3,ship4];
-  
+    
+    ship0.x=50;
+    ship0.y=0;
+    ship1.x=100;
+    ship1.y=50;
+    ship2.x=150;
+    ship2.y=100;
+    ship3.x=200;
+    ship3.y=150;
+    ship4.x=250;
+    ship4.y=200;
+    ship5.x=300;
+    ship5.y=250;
+    
 function _createNewLevel() {
     
     var i;
