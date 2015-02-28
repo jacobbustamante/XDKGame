55a1522b3a4522430841dbc2a7d50b09d4ad86af
Zachary Crandall <zach.t.crandall@gmail.com>
2015-02-28 11:12:12 -0800

    Added ship.js

---
+++ ship.js
@@ -0,0 +1,32 @@
+function Ship() {
+    this.x=0;
+    this.y=0;
+    this.theta=0;
+    this.isPlayer=false;
+    this.health=100;
+    
+    
+function _update() {
+    if(this.isPlayer){
+        
+    }
+    
+    else {
+        
+    }
+}
+    
+function _draw(){
+        
+}
+    
+function _shoot(){
+    
+}
+
+this.update = _update.bind(this);    
+this.update = _draw.bind(this);            
+
+    
+    
+}
