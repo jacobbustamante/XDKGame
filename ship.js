4cb309ea9f1c62e51005c26c04a8d14ce4001439
Zachary Crandall <zach.t.crandall@gmail.com>
2015-02-28 12:45:29 -0800

    Added bullet class

--- ship.js
+++ ship.js
@@ -2,6 +2,8 @@ function Ship() {
     this.x=0;
     this.y=0;
     this.theta=0;
+    this.speed=0;
+    
     this.isPlayer=false;
     this.health=100;
     
@@ -21,11 +23,15 @@ function _draw(){
 }
     
 function _shoot(){
-    
+    var bullet = new Bullet();
+    bullet.x = this.x;
+    bullet.y = this.y;
+    bullet.speed += this.speed;
 }
 
 this.update = _update.bind(this);    
-this.update = _draw.bind(this);            
+this.draw = _draw.bind(this);            
+this.shoot = _shoot.bind(this);            
 
     
     
