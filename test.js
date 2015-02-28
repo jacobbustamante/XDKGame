9ebedd882e4fdb8212e5ade7507f934fc503ef75
Michael Murphy <mmurph18@calpoly.edu>
2015-02-28 03:32:56 -0800

    Added code to test.js for iPhone testing

--- test.js
+++ test.js
@@ -1,9 +1,28 @@
 // use the load event for init code
-// the app global variable will be set
+// the app global variable will be set before this event fires
 window.addEventListener("load", function() {
-    paintGreen();
+    document.body.addEventListener("touchstart", onTap);
+    document.body.addEventListener("touchmove", noAction);
+    document.body.addEventListener("touchend", noAction);
+    document.body.addEventListener("touchcancel", noAction);
 });
 
+function noAction(e) {
+    e.preventDefault();
+}
+                                   
+function onTap(e) {
+    e.preventDefault();
+    var g = app.ctx;
+    for(var i = 0, len = e.touches.length; i < len; ++i) {
+        var t = e.touches[i];
+        var x = t.clientX;
+        var y = t.clientY;
+        var str = "(" + x + "," + y + ")";
+        g.fillText(str, x, y);
+    }
+}
+                                   
 function paintGreen() {
     var ctx = app.ctx;
     var w = app.canvas.width;
